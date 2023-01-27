86c7a85d281bf397610af88950118e53067b3cdc
André Elias <dedekpo@gmail.com>
2023-01-27 13:08:27 -0300

    add: page names - head

---
+++ app/[lang]/about/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+	return (
+		<>
+			<title>Sobre mim</title>
+			<meta
+				content="width=device-width, initial-scale=1"
+				name="viewport"
+			/>
+			<meta name="description" content="Know a little more about me" />
+			<link rel="icon" href="/logo.png" />
+		</>
+	);
+}
---
+++ app/[lang]/portfolio/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+	return (
+		<>
+			<title>Portfolio</title>
+			<meta
+				content="width=device-width, initial-scale=1"
+				name="viewport"
+			/>
+			<meta name="description" content="Take a look at my portfolio" />
+			<link rel="icon" href="/logo.png" />
+		</>
+	);
+}
